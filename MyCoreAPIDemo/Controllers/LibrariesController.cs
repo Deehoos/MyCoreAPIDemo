98f15d2bd24d89cf440249d66edf6a1436a3edd3
Dylan Hughes <dhughes238x@gmail.com>
2019-06-01 13:01:42 +0800

    Add project files.

---
+++ MyCoreAPIDemo/Controllers/LibrariesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyCoreAPIDemo.Repository.Contract;
+
+namespace MyCoreAPIDemo.Controllers
+{
+    [Route("api/Libraries")]
+    [ApiController]
+    public class LibrariesController : ControllerBase
+    {
+        private readonly ILibraryRepository<Author> _libraryRepository;
+
+        public LibrariesController(ILibraryRepository<Author> libraryRepository)
+        {
+            _libraryRepository = libraryRepository;
+        }
+
+        // GET: api/Libraries/GetAllAuthor
+        [HttpGet]
+        [Route("GetAllAuthor")]
+        public IActionResult GetAllAuthor()
+        {
+            IEnumerable<Author> authors = _libraryRepository.GetAllAuthor();
+            return Ok(authors);
+        }
+    }
+}
---
+++ MyCoreAPIDemo/Migrations/20190601044940_MyCoreAPIDemo.Entities.LibraryContextSeed.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyCoreAPIDemo.Migrations
+{
+    public partial class MyCoreAPIDemoEntitiesLibraryContextSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                schema: "dbo",
+                table: "Author",
+                columns: new[] { "AuthorId", "FirstName", "Genre", "LastName" },
+                values: new object[] { new Guid("312f68a4-a401-45b1-883e-51932bee5b19"), "Bob", "Drama", "Ross" });
+
+            migrationBuilder.InsertData(
+                schema: "dbo",
+                table: "Author",
+                columns: new[] { "AuthorId", "FirstName", "Genre", "LastName" },
+                values: new object[] { new Guid("08b36a1d-f2ec-4ba4-b1f2-65391e5ec709"), "David", "Fantasy", "Miller" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "dbo",
+                table: "Author",
+                keyColumn: "AuthorId",
+                keyValue: new Guid("08b36a1d-f2ec-4ba4-b1f2-65391e5ec709"));
+
+            migrationBuilder.DeleteData(
+                schema: "dbo",
+                table: "Author",
+                keyColumn: "AuthorId",
+                keyValue: new Guid("312f68a4-a401-45b1-883e-51932bee5b19"));
+        }
+    }
+}
